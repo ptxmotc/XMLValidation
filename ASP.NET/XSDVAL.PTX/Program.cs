7fc3770dc269509322b6698ac5125af660692ff2
PTX <ptx.motc@gmail.com>
2018-11-09 10:46:50 +0800

    Add files via upload

---
+++ ASP.NET/XSDVAL.PTX/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XSDVAL.PTX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("開始產生Xml檔案");
+            CreateXML();
+            Console.Write("產生Xml檔案結束");
+            Console.ReadKey();
+        }
+
+        static void CreateXML()
+        {
+            List<BusRouteTypeOperator> Operator = new List<BusRouteTypeOperator>
+            {
+                new BusRouteTypeOperator()
+                {
+                    OperatorID = "1100",
+                    OperatorCode ="DananBus",
+                }
+            };
+
+            List<BusRouteType> Routes = new List<BusRouteType>
+            {
+                new BusRouteType()
+                {
+                    RouteID = "17686",
+                    RouteName = new NameType {
+                        Zh_tw="216",
+                        En="216",
+                    },
+                    HasSubRoutes=true,
+                    Operators = Operator.ToArray(),
+                    RouteType = BusRouteTypeEnum.Item11,
+                    ServiceType= new  BusRouteTypeServiceType{
+                        IsFreeBus=false,
+                        IsTaiwanTripBus=false,
+                        IsTourBus=true,
+                        IsTouristBus=false,
+                        IsBRTBus=false,
+                        IsMedicalBus=false,
+                        IsNightBus=true,
+                        IsTrunkBus=true,
+                        IsTHSRShuttleBus=false,
+                        IsTRAShuttleBus=false,
+                        IsAirportShuttleBus=false,
+                        IsActivityShuttleBus=true,
+                    },
+                    TicketPriceDescription = new NameType {
+                         Zh_tw="一段票",
+                         En="1 segment",
+                    },
+                    DepartureStopName =new NameType {
+                         Zh_tw="新北投",
+                         En="Xinbeitou"
+                    },
+                    DestinationStopName=new NameType {
+                        Zh_tw="捷運劍潭站",
+                        En="MRT Jiantan Sta."
+                    },
+                    StartStop= new BusRouteTypeStartStop {
+                        StopID="186328",
+                        StopName=new NameType {
+                            Zh_tw="新北投",
+                            En="Xinbeitou"
+                        },
+                    },
+                    EndStop= new BusRouteTypeEndStop {
+                        StopID="186288",
+                        StopName=new NameType {
+                            Zh_tw="捷運劍潭站",
+                            En="MRT Jiantan Sta."
+                        },
+                    },
+                    IsCircular=true,
+                }
+            };
+            BusRouteListType VDList = new BusRouteListType()
+            {
+                UpdateTime = DateTime.Now,
+                UpdateInterval = 86400,
+                AuthorityCode = "TPE",
+                Routes = Routes.ToArray(),
+            };
+            var Path = System.IO.Path.Combine(Directory.GetParent("..").FullName, @"XSD\Route.xml");
+            Utility.Helper.XMLHelper.Serialize(VDList, Path);
+        }
+    }
+}
---
+++ ASP.NET/XSDVAL.Praking/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XSDVAL.Praking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("開始產生Xml檔案");
+            CreateXML();
+            Console.WriteLine("產生Xml檔案結束");
+            Console.ReadKey();
+        }
+
+        static void CreateXML()
+        {
+            var Availabilitys = new List<AvailabilityType>
+            {
+                new AvailabilityType()
+                {
+                    SpaceType = 1,
+                    NumberOfSpaces =9,
+                    AvailableSpaces = 37,
+                }
+            }.ToArray();
+
+            List<ParkingAvailabilityType> ParkingAvailabilities = new List<ParkingAvailabilityType>
+            {
+                new ParkingAvailabilityType()
+                {
+                    CarParkID = "056",
+                    CarParkName = new NameType {
+                        Zh_tw="大安森林公園地下停車場",
+                    },
+                    Availabilities=Availabilitys,
+                    ServiceStatus = 1,
+                    FullStatus = 0,
+                    ServiceAvailableLevel = 35,
+                    AlmostFullLevel = 50,
+                    FullLevel =55,
+                    OverCrowdingLevel=56,
+                    ChargeStatus=1,
+                    Remark="",
+                }
+            };
+
+            ParkingAvailabilityListType VDList = new ParkingAvailabilityListType()
+            {
+                UpdateTime = DateTime.Now,
+                UpdateInterval = 86400,
+                AuthorityCode = "TPE",
+                ParkingAvailabilities = ParkingAvailabilities.ToArray(),
+            };
+            var Path = System.IO.Path.Combine(Directory.GetParent("..").FullName, @"XSD\ParkingAvailability.xml");
+            Utility.Helper.XMLHelper.Serialize(VDList, Path);
+        }
+    }
+}
